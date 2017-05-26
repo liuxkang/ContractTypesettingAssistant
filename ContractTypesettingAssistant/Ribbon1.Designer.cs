ff28dc1908be2be8c53b53b51c7bc96d3220f5ed
刘康 <liuxkang@hotmail.com>
2017-05-26 18:01:08 +0800

    20170526 Go

---
+++ ContractTypesettingAssistant/Ribbon1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace ContractTypesettingAssistant
+{
+    partial class Ribbon1 : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public Ribbon1()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.group1 = this.Factory.CreateRibbonGroup();
+            this.button1 = this.Factory.CreateRibbonButton();
+            this.group2 = this.Factory.CreateRibbonGroup();
+            this.button2 = this.Factory.CreateRibbonButton();
+            this.button3 = this.Factory.CreateRibbonButton();
+            this.button4 = this.Factory.CreateRibbonButton();
+            this.button5 = this.Factory.CreateRibbonButton();
+            this.button6 = this.Factory.CreateRibbonButton();
+            this.button7 = this.Factory.CreateRibbonButton();
+            this.button8 = this.Factory.CreateRibbonButton();
+            this.button9 = this.Factory.CreateRibbonButton();
+            this.tab1.SuspendLayout();
+            this.group1.SuspendLayout();
+            this.group2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.Groups.Add(this.group1);
+            this.tab1.Groups.Add(this.group2);
+            this.tab1.Label = "合同排版助手";
+            this.tab1.Name = "tab1";
+            // 
+            // group1
+            // 
+            this.group1.Items.Add(this.button1);
+            this.group1.Items.Add(this.button8);
+            this.group1.Label = "全局设置";
+            this.group1.Name = "group1";
+            // 
+            // button1
+            // 
+            this.button1.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.button1.Label = "全局设置";
+            this.button1.Name = "button1";
+            this.button1.ShowImage = true;
+            this.button1.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button1_Click);
+            // 
+            // group2
+            // 
+            this.group2.Items.Add(this.button2);
+            this.group2.Items.Add(this.button3);
+            this.group2.Items.Add(this.button4);
+            this.group2.Items.Add(this.button5);
+            this.group2.Items.Add(this.button6);
+            this.group2.Items.Add(this.button7);
+            this.group2.Items.Add(this.button9);
+            this.group2.Label = "格式设置";
+            this.group2.Name = "group2";
+            // 
+            // button2
+            // 
+            this.button2.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.button2.Label = "主标题";
+            this.button2.Name = "button2";
+            this.button2.ShowImage = true;
+            this.button2.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.button3.Label = "编号部分";
+            this.button3.Name = "button3";
+            this.button3.ShowImage = true;
+            // 
+            // button4
+            // 
+            this.button4.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.button4.Label = "首页页底";
+            this.button4.Name = "button4";
+            this.button4.ShowImage = true;
+            // 
+            // button5
+            // 
+            this.button5.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.button5.Label = "正文文本";
+            this.button5.Name = "button5";
+            this.button5.ShowImage = true;
+            // 
+            // button6
+            // 
+            this.button6.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.button6.Label = "章节";
+            this.button6.Name = "button6";
+            this.button6.ShowImage = true;
+            // 
+            // button7
+            // 
+            this.button7.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.button7.Label = "表格";
+            this.button7.Name = "button7";
+            this.button7.ShowImage = true;
+            // 
+            // button8
+            // 
+            this.button8.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.button8.Label = "定制首页";
+            this.button8.Name = "button8";
+            this.button8.ShowImage = true;
+            // 
+            // button9
+            // 
+            this.button9.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.button9.Label = "页眉";
+            this.button9.Name = "button9";
+            this.button9.ShowImage = true;
+            // 
+            // Ribbon1
+            // 
+            this.Name = "Ribbon1";
+            this.RibbonType = "Microsoft.Word.Document";
+            this.Tabs.Add(this.tab1);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.Ribbon1_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.group1.ResumeLayout(false);
+            this.group1.PerformLayout();
+            this.group2.ResumeLayout(false);
+            this.group2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button8;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group2;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button2;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button3;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button4;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button5;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button6;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button7;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button9;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal Ribbon1 Ribbon1
+        {
+            get { return this.GetRibbon<Ribbon1>(); }
+        }
+    }
+}
---
+++ ContractTypesettingAssistant/Ribbon1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+using Microsoft.Office.Interop.Word;
+using System.Windows.Forms;
+
+namespace ContractTypesettingAssistant
+{
+    public partial class Ribbon1
+    {
+        private Microsoft.Office.Interop.Word.Application WordApp;
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+            WordApp = Globals.ThisAddIn.Application;
+        }
+
+        //设置合同模板全局，增加上下左右边距等
+        private void button1_Click(object sender, RibbonControlEventArgs e)
+        {
+            //设置上下左右边距
+            WordApp.ActiveDocument.PageSetup.TopMargin = 60;
+            WordApp.ActiveDocument.PageSetup.BottomMargin = 60;
+            WordApp.ActiveDocument.PageSetup.LeftMargin = 80;
+            WordApp.ActiveDocument.PageSetup.RightMargin = 80;
+
+            //全局大纲变为“正文”
+            WordApp.ActiveDocument.Paragraphs.Format.OutlineLevel = WdOutlineLevel.wdOutlineLevelBodyText;
+        }
+
+        private void button2_Click(object sender, RibbonControlEventArgs e)
+        {
+            Style style = null;
+            //添加和设置正文格式
+            try
+            {
+                style = WordApp.ActiveDocument.Styles["正文"];
+            }
+            catch(Exception)
+            {
+                WordApp.ActiveDocument.Styles.Add("正文");
+                style = WordApp.ActiveDocument.Styles["正文"];
+            }
+            style.ParagraphFormat.OutlineLevel = WdOutlineLevel.wdOutlineLevelBodyText;     //大纲“正文”文本
+            style.ParagraphFormat.CharacterUnitFirstLineIndent = 2;                         //首行缩进两个字符
+            style.ParagraphFormat.LineSpacing = 24;                                         //行距24磅
+
+            style.Font.NameFarEast = "仿宋";
+            style.Font.NameAscii = "Times New Roman";
+            style.Font.NameOther = "Times New Roman";
+            style.Font.Name = "正文";
+
+        }
+       
+    }
+}
