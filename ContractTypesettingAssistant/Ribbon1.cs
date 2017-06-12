ff9a68b8653e3d0db9fe843114277ec704b5eaba
刘康 <liuxkang@hotmail.com>
2017-06-12 12:14:44 +0800

    2017-06-12 早上编写

--- ContractTypesettingAssistant/Ribbon1.cs
+++ ContractTypesettingAssistant/Ribbon1.cs
@@ -27,11 +27,12 @@ namespace ContractTypesettingAssistant
         private void button1_Click(object sender, RibbonControlEventArgs e)
         {
             //创建各种样式
-            create_style_zhengwen();
-            create_style_biaoti();
-            create_style_bianhao();
-            create_style_zhangjie();
-            create_style_table();
+            create_style_zhengwen();            //插件启动时创建 正文 格式
+            create_style_biaoti();              //插件启动时创建 合同标题 格式
+            create_style_bianhao();             //插件启动时创建 编号部分 格式
+            create_style_zhangjie();            //插件启动时创建 合同章节 格式
+            create_style_table();               //插件启动时创建 合同表格 格式
+            create_style_yemei();               //插件启动时创建 合同页眉 格式
 
             //设置上下左右边距
             WordApp.ActiveDocument.PageSetup.TopMargin = 70;
@@ -78,6 +79,7 @@ namespace ContractTypesettingAssistant
         //设置主标题格式
         private void button2_Click(object sender, RibbonControlEventArgs e)
         {
+            WordApp.Selection.ClearFormatting();
             WordApp.Selection.set_Style("合同主标题");
         }
 
@@ -100,12 +102,14 @@ namespace ContractTypesettingAssistant
             style.ParagraphFormat.CharacterUnitFirstLineIndent = 2;                         //首行缩进两个字符
             style.ParagraphFormat.LineSpacingRule = WdLineSpacing.wdLineSpaceExactly;       //行距设置为固定值
             style.ParagraphFormat.LineSpacing = 24;                                         //行距24磅
+            style.ParagraphFormat.Alignment = WdParagraphAlignment.wdAlignParagraphLeft;    //左对齐
 
             style.Font.NameFarEast = "仿宋";                                                //中文字体
             style.Font.NameAscii = "仿宋";                                                  //英文格式
             style.Font.NameOther = "仿宋";                                                  //字符格式
             style.Font.Name = "仿宋";                                                       //格式名称
-            style.Font.Size = 12;
+            style.Font.Size = 14;                                                           //正文字号"四号"
+            style.Font.Bold = 0;
 
         }
 
@@ -126,6 +130,7 @@ namespace ContractTypesettingAssistant
             style.set_BaseStyle("正文");
             style.ParagraphFormat.OutlineLevel = WdOutlineLevel.wdOutlineLevel2;
             style.set_NextParagraphStyle("正文");
+            style.Font.Size = 14;
             style.Font.Bold = 1;
 
         }
@@ -151,12 +156,14 @@ namespace ContractTypesettingAssistant
             style.set_BaseStyle("");                                               //设置基准样式为（无样式）
             style.set_NextParagraphStyle("正文");                                   //设置后续样式为“正文”
             style.ParagraphFormat.FirstLineIndent = 0;                           //首行缩进0个字符
+            style.ParagraphFormat.Alignment = WdParagraphAlignment.wdAlignParagraphLeft;    //左对齐
 
             style.Font.NameFarEast = "方正小标宋简体";                           //中文字体
             style.Font.NameAscii = "仿宋";                                       //英文格式
             style.Font.NameOther = "仿宋";                                       //字符格式
             style.Font.Name = "方正小标宋简体";                                  //格式名称
             style.Font.Size = 40;                                                //设置字体大小
+            style.Font.Bold = 0;
         }
 
         //创建合同编号部分的字体样式
@@ -177,25 +184,28 @@ namespace ContractTypesettingAssistant
             style.set_BaseStyle("");                                                        //设置基准样式为（无样式）
             style.set_NextParagraphStyle("正文");                                           //设置后续样式为“正文”
             style.ParagraphFormat.FirstLineIndent = 0;                                      //首行缩进0个字符
+            style.ParagraphFormat.Alignment = WdParagraphAlignment.wdAlignParagraphLeft;    //左对齐
 
             style.Font.NameFarEast = "仿宋";                                                //中文字体
             style.Font.NameAscii = "仿宋";                                                  //英文格式
-            style.Font.NameOther = "仿宋";                                       //字符格式
-            style.Font.Name = "仿宋";                                             //格式名称
-            style.Font.Size = 17;                                                //设置字体大小
-            style.Font.Bold = 1;                                                 //设置为粗体
+            style.Font.NameOther = "仿宋";                                                  //字符格式
+            style.Font.Name = "仿宋";                                                       //格式名称
+            style.Font.Size = 17;                                                           //设置字体大小
+            style.Font.Bold = 1;                                                            //设置为粗体
 
         }
 
         //设置编号部分字体
         private void button3_Click(object sender, RibbonControlEventArgs e)
         {
+            WordApp.Selection.ClearCharacterAllFormatting();
             WordApp.Selection.set_Style("编号部分");
         }
 
         //首页页底签订时间 签订地点部分
         private void button4_Click(object sender, RibbonControlEventArgs e)
         {
+            WordApp.Selection.ClearFormatting();
             WordApp.Selection.TypeText("  签订时间：201 年   月   日");
             WordApp.Selection.set_Style("正文");
             WordApp.Selection.TypeParagraph();
@@ -223,30 +233,67 @@ namespace ContractTypesettingAssistant
             style.ParagraphFormat.LineSpacingRule = WdLineSpacing.wdLineSpaceSingle;              //单倍行距
             style.ParagraphFormat.FirstLineIndent = 0;                                            //首行缩进0个字符
             style.ParagraphFormat.OutlineLevel = WdOutlineLevel.wdOutlineLevelBodyText;           //大纲级别正文文本
+            style.ParagraphFormat.Alignment = WdParagraphAlignment.wdAlignParagraphLeft;          //左对齐
 
             style.Font.NameFarEast = "仿宋";                                                      //中文字体
             style.Font.NameAscii = "仿宋";                                                        //英文格式
             style.Font.NameOther = "仿宋";                                                        //字符格式
             style.Font.Name = "仿宋";                                                             //格式名称
             style.Font.Size = 12;                                                                 //设置字体大小 小四号
+            style.Font.Bold = 0;                                                                  //不加粗
+
+        }
+
+        //创建合同页眉
+        private void create_style_yemei()
+        {
+            Style style = null;
+            try
+            {
+                style = WordApp.ActiveDocument.Styles["合同页眉"];
+            }
+            catch (Exception)
+            {
+                WordApp.ActiveDocument.Styles.Add("合同页眉");
+                style = WordApp.ActiveDocument.Styles["合同页眉"];
+            }
+            style.set_BaseStyle("");
+            style.ParagraphFormat.LineSpacingRule = WdLineSpacing.wdLineSpaceExactly;               //行距为固定值
+            style.ParagraphFormat.LineSpacing = 12;                                                 //行距为12磅
+            style.set_NextParagraphStyle("合同页眉");
+            style.ParagraphFormat.Alignment = WdParagraphAlignment.wdAlignParagraphLeft;            //左对齐
+
+            style.Font.NameFarEast = "仿宋";                                                      //中文字体
+            style.Font.NameAscii = "仿宋";                                                        //英文格式
+            style.Font.NameOther = "仿宋";                                                        //字符格式
+            style.Font.Name = "仿宋";                                                             //格式名称
+            style.Font.Size = 9;                                                                 //设置字体大小 小五号
+            style.Font.Bold = 0;
 
         }
 
         //合同章节部分
         private void button6_Click(object sender, RibbonControlEventArgs e)
         {
+            WordApp.Selection.ClearFormatting();
             WordApp.Selection.set_Style("合同章节");
         }
 
         //合同表格
         private void button7_Click(object sender, RibbonControlEventArgs e)
         {
+            WordApp.Selection.ClearFormatting();
             WordApp.Selection.set_Style("合同表格");
         }
 
         //合同页眉
         private void button8_Click(object sender, RibbonControlEventArgs e)
         {
+            WordApp.ActiveWindow.View.SeekView = WdSeekView.wdSeekCurrentPageHeader;
+            WordApp.Selection.HeaderFooter.LinkToPrevious = true;
+            WordApp.Selection.HeaderFooter.Range.ParagraphFormat.Alignment = WdParagraphAlignment.wdAlignParagraphRight;
+
+            WordApp.Selection.set_Style("合同页眉");
 
         }
     }
